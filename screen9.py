193c65d9075431f0c7db0e1c7f1ddcdb3e8eb630
Phil Jeffes <philjeffes@hotmail.com>
2016-10-27 21:48:11 +0100

    Added next two questions

--- screen9.py
+++ screen9.py
@@ -1,7 +1,7 @@
 import pygame
-from screen8 import Screen8
+# from screen10 import Screen10
 
-class Screen7(object):
+class Screen9(object):
     def __init__(self):
         self.last_answer = None
         self.last_answer_value = None
@@ -11,37 +11,37 @@ class Screen7(object):
     def draw(self, screen):
         #If last_answer is False then print the error message
         font = pygame.font.Font(None, 36)
-        text = font.render("Question 7", 1, (255, 255, 0))
+        text = font.render("Question 9", 1, (255, 255, 0))
         textpos = text.get_rect()
         textpos.left = 200
         screen.blit(text, textpos)
 
-        question = font.render(" Which of the following is the most effective ", 1, (255, 255, 0))
+        question = font.render("Which of the following ISN’T a common medium", 1, (255, 255, 0))
         q_pos = question.get_rect()
         q_pos.top = 65
         screen.blit(question, q_pos)
 
-        q2 = font.render("way to stop a zombie?", 1, (255, 255,0))
+        q2 = font.render("used in the rituals of Witches?", 1, (255, 255,0))
         q2_pos = q2.get_rect()
         q2_pos.top = q_pos.top + 35
         screen.blit(q2, q2_pos)
 
-        a1 = font.render("a. Set them on fire", 1, (255, 255, 0))
+        a1 = font.render("a. Cellphones", 1, (255, 255, 0))
         a1_pos = a1.get_rect()
         a1_pos.top = q2_pos.top + 45
         screen.blit(a1, a1_pos)
 
-        a2 = font.render("b. Shoot them in the hear", 1, (255, 255, 0))
+        a2 = font.render("b. Their Hair", 1, (255, 255, 0))
         a2_pos = a2.get_rect()
         a2_pos.top = a1_pos.top + 35
         screen.blit(a2, a2_pos)
 
-        a3 = font.render("c. Destroy their brains", 1, (255, 255, 0))
+        a3 = font.render("c. Honey", 1, (255, 255, 0))
         a3_pos = a3.get_rect()
         a3_pos.top = a2_pos.top + 35
         screen.blit(a3, a3_pos)
 
-        a4 = font.render("d. Poison them", 1, (255, 255, 0))
+        a4 = font.render("d. Candles", 1, (255, 255, 0))
         a4_pos = a4.get_rect()
         a4_pos.top = a3_pos.top + 35
         screen.blit(a4, a4_pos)
@@ -60,22 +60,23 @@ class Screen7(object):
             return
 
         #If we answered correctly then return True otherwise false
-        if keys[pygame.K_c]:
+        if keys[pygame.K_a]:
             return True
-        elif keys[pygame.K_b] or keys[pygame.K_d] or keys[pygame.K_a]:
+        elif keys[pygame.K_b] or keys[pygame.K_c] or keys[pygame.K_d]:
             self.last_answer = False
             self.cooldown_timer = self.MAX_TIMER
             if keys[pygame.K_b]:
                 self.last_answer_value = 'b'
+            elif keys[pygame.K_c]:
+                self.last_answer_value = 'c'
             elif keys[pygame.K_d]:
                 self.last_answer_value = 'd'
-            elif keys[pygame.K_a]:
-                self.last_answer_value = 'a'
 
         return False
 
     def next(self):
-        return Screen8()
+        return None
+        # return Screen8()
 
     def get_error_message(self):
         return "That is incorrect try again"
